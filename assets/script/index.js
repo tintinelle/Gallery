591af2e7a2e496248a7547e784f25db054f44023
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-08-13 15:05:40 +0500

    finished gallery

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,9 +1,16 @@
+"use strict";
+
 const changeImageNext = () => {
     const characterImage = document.getElementById('characterImage');
     characterImage.src = "assets/img/ciri.jpeg";
+
+    const characterName = document.getElementById('characterName');
+    characterName.innerHTML = "Cirilla";
 }
 
 const changeImageBack = () => {
     const characterImage = document.getElementById('characterImage');
     characterImage.src = "assets/img/geralt.jpg";
+    const characterName = document.getElementById('characterName');
+    characterName.innerHTML = "Geralt from Rivia";
 }
