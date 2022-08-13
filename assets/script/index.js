e37815fb37a3dc019098bd4db1045b80b9db9369
Ilvina Rakhimova <ilvrakh@gmail.com>
2022-08-13 11:46:01 +0500

    started gallery

---
+++ assets/script/index.js
@@ -0,0 +1,9 @@
+const changeImageNext = () => {
+    const characterImage = document.getElementById('characterImage');
+    characterImage.src = "assets/img/ciri.jpeg";
+}
+
+const changeImageBack = () => {
+    const characterImage = document.getElementById('characterImage');
+    characterImage.src = "assets/img/geralt.jpg";
+}
